3b1cb967de7cb40cfc126ff353c343d1c17def7c
alexa <alexanderseasone@gmail.com>
2018-05-07 21:00:32 +0300

    vrode nichego ne polomal

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function show_team() {
-	JsHttpRequest.query('/content.php',{'w':'show_team'}, 
-	function (result, errors){ if (errors) {alert(errors);} if (result){  
-		alert(result["content"]);
-	} else console.log("error"); }, true);
-}
--- old_stuff/content.php
+++ old_stuff/content.php
@@ -1,7 +1,7 @@
 <?php
 
 define('RD', dirname (__FILE__));
-require_once (RD."/lib/team_class.php"); $team=new team;
+require_once(RD . "/lib/team_class.php"); $team=new team;
 require_once (RD."/js/JsHttpRequest/JsHttpRequest.php"); $JsHttpRequest =& new JsHttpRequest("windows-1251");
 
 if ($_REQUEST["w"]=="show_team"){ 
