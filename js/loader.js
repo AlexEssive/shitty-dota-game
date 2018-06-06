dfdfd4f418d7b73d97d6b78526be3eddf77e907e
alexa <alexanderseasone@gmail.com>
2018-06-06 20:32:04 +0300

    tipa krasivo

--- js/loader.js
+++ js/loader.js
@@ -1,22 +1,38 @@
-//load libs
+/**
+ * Scripts loader
+ *
+ * Підключати скріпти тут
+ * Порядок: plugins->libs->data->game
+ * При другому порядку може вилізти пісюн в консоль
+ * Не пхати всяку хєрню в рандомну категорію, потім без бутилки не розберешся
+ */
+
+/***********************************************/
+
+/*** load libs (плагіни) ***/
 loadScript("plugins/bootstrap.min.js");
 
-//load data
+/*** load libs (самописні інструменти, скріпти, які напряму не юзаються в грі) ***/
+loadScript("libs/modal.js");
+loadScript("libs/cookies.js");
+loadScript("libs/upload.js");
+loadScript("libs/context.js");
+
+/*** load data (всьо, шо касається джсон дати і кукі менеджмента) ***/
 loadScript("data/data.js");
 
-//load game scripts
+/*** load game scripts (ігрові скріпти) ***/
 loadScript("game/main.js");
 loadScript("game/players.js");
 loadScript("game/days.js");
 loadScript("game/market.js");
 loadScript("game/team.js");
 loadScript("game/matches.js");
-loadScript("game/context.js");
 loadScript("game/user.js");
-loadScript("game/upload.js");
-loadScript("game/cookies.js");
-loadScript("game/modal.js");
 
+/***********************************************/
+
+//дуже сложна функція
 function loadScript(script) {
     $.getScript('../js/' + script);
 }
